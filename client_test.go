f07306eb2a63cdaab6513b7257756f70897a1f4d
jared <jared.marks@live.com>
2024-04-19 16:39:41 -0400

    Updated cache and client

--- client_test.go
+++ client_test.go
@@ -1,11 +1,48 @@
 package main
 
 import (
-	"testing"
-
-	"github.com/bwlee13/gopherdb/gopherdb"
+    "encoding/binary"
+    "fmt"
+    "net"
+    "os"
 )
 
-func TestHandler(t *testing.T) {
-	gopherdb.HandleConn()
+func main() {
+    conn, err := net.Dial("tcp", "localhost:42069")
+    if err != nil {
+        fmt.Println("Failed to connect to server:", err)
+        os.Exit(1)
+    }
+    defer conn.Close()
+
+    // Simulate sending a string
+    sendString(conn, "hello world")
+}
+
+func sendString(conn net.Conn, s string) {
+    // Convert the string to bytes
+    data := []byte(s)
+
+    // Send the length of the data as a uint32
+    err := binary.Write(conn, binary.BigEndian, uint32(len(data)))
+    if err != nil {
+        fmt.Println("Error writing length:", err)
+        return
+    }
+
+    // Send the actual data
+    _, err = conn.Write(data)
+    if err != nil {
+        fmt.Println("Error writing data:", err)
+        return
+    }
+
+    // Wait for acknowledgment
+    ack := make([]byte, 4)
+    if _, err = conn.Read(ack); err != nil {
+        fmt.Println("Error reading acknowledgment:", err)
+        return
+    }
+
+    fmt.Println("Acknowledgment received:", string(ack))
 }
--- gopherdb/cache.go
+++ gopherdb/cache.go
@@ -1,8 +1,54 @@
 package gopherdb
 
-import "fmt"
+import (
+    "fmt"
+    "net"
+)
 
+// Correct the struct definition
+type Cache struct {
+    conn net.Conn
+}
+
+// Correct NewCache to accept serverAddress and return an error
+func NewCache(serverAddress string) (*Cache, error) {
+    conn, err := net.Dial("tcp", serverAddress)
+    if err != nil {
+        return nil, err
+    }
+    return &Cache{conn: conn}, nil
+}
+
+// Add parameters and correct the return type for Set
+func (c *Cache) Set(key string, value string) error {
+    _, err := c.conn.Write([]byte(fmt.Sprintf("SET %s %s\n", key, value)))
+    return err
+}
+
+// Add parameters and correct the return type for Get
+func (c *Cache) Get(key string) (string, error) {
+    _, err := c.conn.Write([]byte(fmt.Sprintf("GET %s\n", key)))
+    if err != nil {
+        return "", err
+    }
+
+    // Read response
+    buffer := make([]byte, 1024)
+    n, err := c.conn.Read(buffer)
+    if err != nil {
+        return "", err
+    }
+
+    return string(buffer[:n]), nil
+}
+
+// Check for errors on close
+func (c *Cache) Close() error {
+    return c.conn.Close()
+}
+
+// Define or import GetTimestamp if it's used
 func HandleConn() {
-	fmt.Println("Hanldeing conn...")
-	GetTimestamp()
+    fmt.Println("Handling conn...")
+    // GetTimestamp()  // This function needs to be defined or imported
 }
