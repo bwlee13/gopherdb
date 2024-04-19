3f01a997024a783ef913204f1bea7ee4967a5f7f
bwlee13 <brandonlee2013@gmail.com>
2024-04-19 13:24:08 -0400

    initial tcp

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
 	// entry point to whole app
 	fmt.Println("Here we goooo")
+
+	serve, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		fmt.Println("Conn Err TCP: ", err)
+		panic("PANIC ON CONN ")
+	}
+
+	for {
+		conn, err := serve.Accept()
+
+		if err != nil {
+			fmt.Println("Conn Err TCP: ", err)
+			panic("PANIC ON CONN ")
+		}
+		handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	fmt.Println("Hanldeing conn...")
+	fmt.Println(conn)
 }
